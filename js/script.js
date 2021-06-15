10c9762eb42cc76060bbd47ae570feb79a15ad67
vishvesh patel <vishvesh2098@gmail.com>
2021-06-15 11:42:37 +0530

    added

---
+++ js/script.js
@@ -0,0 +1,139 @@
+function oneclicked(){
+    document.getElementById("one").classList.add("active-btn");
+    document.getElementById("two").classList.remove("active-btn");
+    document.getElementById("three").classList.remove("active-btn");
+    document.getElementById("four").classList.remove("active-btn");
+    document.getElementById("five").classList.remove("active-btn");
+    document.getElementById("six").classList.remove("active-btn");
+    document.getElementById("seven").classList.remove("active-btn");
+
+    document.getElementById("first-sem").setAttribute("href", "files/sem1.html");
+    document.getElementById("second-sem").setAttribute("href", "files/sem1.html");
+    document.getElementById("third-sem").setAttribute("href", "files/sem1.html");
+    document.getElementById("fourth-sem").setAttribute("href", "files/sem1.html");
+    document.getElementById("fifth-sem").setAttribute("href", "files/sem1.html");
+    document.getElementById("sixth-sem").setAttribute("href", "files/sem1.html");
+    document.getElementById("seventh-sem").setAttribute("href", "files/sem1.html");
+    document.getElementById("eightth-sem").setAttribute("href", "files/sem1.html");
+}
+function twoclicked(){
+    document.getElementById("one").classList.remove("active-btn");
+    document.getElementById("two").classList.add("active-btn");
+    document.getElementById("three").classList.remove("active-btn");
+    document.getElementById("four").classList.remove("active-btn");
+    document.getElementById("five").classList.remove("active-btn");
+    document.getElementById("six").classList.remove("active-btn");
+    document.getElementById("seven").classList.remove("active-btn");
+    
+    // value of href attribute will change
+
+    document.getElementById("first-sem").setAttribute("href", "");
+
+    document.getElementById("second-sem").setAttribute("href", "");
+    document.getElementById("third-sem").setAttribute("href", "");
+    document.getElementById("fourth-sem").setAttribute("href", "");
+    document.getElementById("fifth-sem").setAttribute("href", "");
+    document.getElementById("sixth-sem").setAttribute("href", "");
+    document.getElementById("seventh-sem").setAttribute("href", "");
+    document.getElementById("eightth-sem").setAttribute("href", "");
+}
+function threeclicked(){
+    document.getElementById("one").classList.remove("active-btn");
+    document.getElementById("two").classList.remove("active-btn");
+    document.getElementById("three").classList.add("active-btn");
+    document.getElementById("four").classList.remove("active-btn");
+    document.getElementById("five").classList.remove("active-btn");
+    document.getElementById("six").classList.remove("active-btn");
+    document.getElementById("seven").classList.remove("active-btn");
+    
+    // value of href attribute will change
+
+    document.getElementById("first-sem").setAttribute("href", "");
+    document.getElementById("second-sem").setAttribute("href", "");
+    document.getElementById("third-sem").setAttribute("href", "");
+    document.getElementById("fourth-sem").setAttribute("href", "");
+    document.getElementById("fifth-sem").setAttribute("href", "");
+    document.getElementById("sixth-sem").setAttribute("href", "");
+    document.getElementById("seventh-sem").setAttribute("href", "");
+    document.getElementById("eightth-sem").setAttribute("href", "");
+}
+function fourclicked(){
+    document.getElementById("one").classList.remove("active-btn");
+    document.getElementById("two").classList.remove("active-btn");
+    document.getElementById("three").classList.remove("active-btn");
+    document.getElementById("four").classList.add("active-btn");
+    document.getElementById("five").classList.remove("active-btn");
+    document.getElementById("six").classList.remove("active-btn");
+    document.getElementById("seven").classList.remove("active-btn");
+    
+    // value of href attribute will change
+
+    document.getElementById("first-sem").setAttribute("href", "");
+    document.getElementById("second-sem").setAttribute("href", "");
+    document.getElementById("third-sem").setAttribute("href", "");
+    document.getElementById("fourth-sem").setAttribute("href", "");
+    document.getElementById("fifth-sem").setAttribute("href", "");
+    document.getElementById("sixth-sem").setAttribute("href", "");
+    document.getElementById("seventh-sem").setAttribute("href", "");
+    document.getElementById("eightth-sem").setAttribute("href", "");
+}
+function fiveclicked(){
+    document.getElementById("one").classList.remove("active-btn");
+    document.getElementById("two").classList.remove("active-btn");
+    document.getElementById("three").classList.remove("active-btn");
+    document.getElementById("four").classList.remove("active-btn");
+    document.getElementById("five").classList.add("active-btn");
+    document.getElementById("six").classList.remove("active-btn");
+    document.getElementById("seven").classList.remove("active-btn");
+    // value of href attribute will change
+
+    document.getElementById("first-sem").setAttribute("href", "");
+    document.getElementById("second-sem").setAttribute("href", "");
+    document.getElementById("third-sem").setAttribute("href", "");
+    document.getElementById("fourth-sem").setAttribute("href", "");
+    document.getElementById("fifth-sem").setAttribute("href", "");
+    document.getElementById("sixth-sem").setAttribute("href", "");
+    document.getElementById("seventh-sem").setAttribute("href", "");
+    document.getElementById("eightth-sem").setAttribute("href", "");
+}
+function sixclicked(){
+    document.getElementById("one").classList.remove("active-btn");
+    document.getElementById("two").classList.remove("active-btn");
+    document.getElementById("three").classList.remove("active-btn");
+    document.getElementById("four").classList.remove("active-btn");
+    document.getElementById("five").classList.remove("active-btn");
+    document.getElementById("six").classList.add("active-btn");
+    document.getElementById("seven").classList.remove("active-btn");
+    
+    // value of href attribute will change
+
+    document.getElementById("first-sem").setAttribute("href", "");
+    document.getElementById("second-sem").setAttribute("href", "");
+    document.getElementById("third-sem").setAttribute("href", "");
+    document.getElementById("fourth-sem").setAttribute("href", "");
+    document.getElementById("fifth-sem").setAttribute("href", "");
+    document.getElementById("sixth-sem").setAttribute("href", "");
+    document.getElementById("seventh-sem").setAttribute("href", "");
+    document.getElementById("eightth-sem").setAttribute("href", "");
+}
+function sevenclicked(){
+    document.getElementById("one").classList.remove("active-btn");
+    document.getElementById("two").classList.remove("active-btn");
+    document.getElementById("three").classList.remove("active-btn");
+    document.getElementById("four").classList.remove("active-btn");
+    document.getElementById("five").classList.remove("active-btn");
+    document.getElementById("six").classList.remove("active-btn");
+    document.getElementById("seven").classList.add("active-btn");
+    
+    // value of href attribute will change
+
+    document.getElementById("first-sem").setAttribute("href", "");
+    document.getElementById("second-sem").setAttribute("href", "");
+    document.getElementById("third-sem").setAttribute("href", "");
+    document.getElementById("fourth-sem").setAttribute("href", "");
+    document.getElementById("fifth-sem").setAttribute("href", "");
+    document.getElementById("sixth-sem").setAttribute("href", "");
+    document.getElementById("seventh-sem").setAttribute("href", "");
+    document.getElementById("eightth-sem").setAttribute("href", "");
+}
+
